a690dbb7f8b681ec103e89519b3cae777abe212a
tad1 <tadziobrzeski@gmail.com>
2024-01-07 22:56:07 +0100

    initial

--- src/main.rs
+++ src/main.rs
@@ -18,37 +18,10 @@ pub fn main(){
     // Currently a boilerplate that I'm willing to integrate automate with linker script, and base assembly
     KERNEL_GATE.set(unsafe {get_kernel_gate()});
 
-    println!("loaded!");
-    
     let mut volume_controller = VolumeManager::new(&SDCARD, TestClock{});
     let volume0 = volume_controller.open_volume(VolumeIdx(0)).unwrap();
     let dir = volume_controller.open_root_dir(volume0).unwrap();
-    let file_res = volume_controller.open_file_in_dir(dir, "appl", embedded_sdmmc::Mode::ReadOnly);
-    print!("\x1b[2J");
-
-    if let Ok(file) = file_res {
-        
-
-        const HEIGHT: usize = 30;
-        const WIDTH: usize = 100;
-        let mut buffer: [u8; WIDTH] = [b' '; WIDTH];
-
-        let _ = volume_controller.file_seek_from_start(file, 0);
-        
-        for i in 0..65430{
-            if i % HEIGHT == 0{
-                kernel_call_a(common::KernelFunction::Spin, 50);
-                print!("\x1b[H");
-            }
-            
-            let n_bytes = volume_controller.read(file, &mut buffer).unwrap();
-            let str: &str = unsafe {transmute(&buffer[..n_bytes] as &[u8])};
-            println!("{}",str);
-        }
-        
-        let _ = volume_controller.close_file(file);
-    } else {
-        println!("File appl not found!");
-    }
-    
+    let _ = volume_controller.iterate_dir(dir, |entry| {
+        println!("\t{}",entry.name);
+    });
 }
